1bc9bb31b930677a962286d1c8f496caf7807c6e
lifelinecoding <apatel89329@gmail.com>
2026-08-10 10:59:23 +0530

    Searching: Pending linear search with multiple elements

--- Arrays/Searching/LinearSearch.c
+++ Arrays/Searching/LinearSearch.c
@@ -18,7 +18,7 @@ int main()
     int arr[] = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10};
 
     int size = sizeof(arr) / sizeof(int);
-    int target = 11;
+    int target = 1;
 
     int result = LinearSearch(arr, size, target);
 
