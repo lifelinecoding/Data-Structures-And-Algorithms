733a3e37171d3e7563bb65b5ee3f0ca22e8e22e0
lifelinecoding <apatel89329@gmail.com>
2026-08-19 09:57:38 +0530

    Array Insertion: Inserting a new element at starting/particular index.

--- Arrays/Insertion/InsertionWithoutOrder.c
+++ Arrays/Insertion/InsertionWithoutOrder.c
@@ -16,6 +16,37 @@ void InsertAtEnd(int arr[], int *size, int capacity, int newItem)
     }
 }
 
+// Insertion at the starting.
+void InsertAtStart(int arr[], int *size, int capacity, int newItem){
+    if(*size >= capacity){
+        printf("Array is full.\n");
+    }
+    else{
+        int temp = arr[0];
+        arr[0] = newItem;
+        arr[*size] = temp;
+
+        *size = *size + 1;
+    }
+}
+
+// Insertion at the particular index.
+void InsertAtParticularIndex(int arr[], int *size, int capacity, int index, int newItem){
+    if(*size >= capacity){
+        printf("Array is full.\n");
+    }    
+    else if(index > *size){
+        printf("Invalid index.\n");
+    }
+    else {
+        int temp = arr[index];
+        arr[index] = newItem;
+        arr[*size] = temp;
+        *size = *size + 1;
+    }
+}
+
+
 // Function to display array elements
 void Display(int arr[], int *size)
 {
@@ -35,14 +66,15 @@ int main()
     InsertAtEnd(arr, &size, capacity, 10);
     InsertAtEnd(arr, &size, capacity, 5);
     InsertAtEnd(arr, &size, capacity, 6);
-    InsertAtEnd(arr, &size, capacity, 0);
-    InsertAtEnd(arr, &size, capacity, 7);
-    InsertAtEnd(arr, &size, capacity, 17);
-    InsertAtEnd(arr, &size, capacity, 8);
-    InsertAtEnd(arr, &size, capacity, 6);
-    InsertAtEnd(arr, &size, capacity, 3);
-    InsertAtEnd(arr, &size, capacity, 1);
-    InsertAtEnd(arr, &size, capacity, 100);
+
+    InsertAtStart(arr, &size, capacity, 2);
+    InsertAtStart(arr, &size, capacity, 12);
+
+    InsertAtParticularIndex(arr, &size, capacity, 2, 3);
+    InsertAtParticularIndex(arr, &size, capacity, 1, 0);
+
+
+    // 10 5 6 -> 2 5 6 10 -> 12 5 6 10 2 -> 12 5 3 10 2 6 -> 12 0 3 10 2 6 5
 
     // printf("%d", size);
 
